f0b564c6bb1015bf8a0f0dfdaa2d4c4210f37399
DESKTOP-GFB4VK6\ues <majed.cseru@gmail.com>
2025-02-19 00:03:56 -0500

    Commit Implementation of service and repository GetCourses

---
+++ CleanArchitecture/CleanArchitecture.Application/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository _courseReporsitory;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseReporsitory = courseRepository;
+        }
+        public CourseViewModel GetCourse()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseReporsitory.GetCourses(),
+            }; 
+        }
+    }
+}
