cb9cdd9e65ef08f37e5b90d5c74b79087842734d
DESKTOP-GFB4VK6\ues <majed.cseru@gmail.com>
2025-03-09 23:16:28 -0400

    Adding Domain.Core and Implementation InMemory.Bus

--- CleanArchitecture/CleanArchitecture.Infra.Bus/InMemoryBus.cs
+++ CleanArchitecture/CleanArchitecture.Infra.Bus/InMemoryBus.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using CleanArchitecture.Domain.Core.Bus;
+using CleanArchitecture.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CleanArchitecture.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus: IMediatorHandler
     {
+        private readonly IMediator _iMediator;
+        public InMemoryBus(IMediator iMediator)
+        {
+            _iMediator = iMediator;
+        }
+
+        public Task SendCommand<T>(T command) where T: Command
+        {
+            return _iMediator.Send(command);
+        }
     }
 }
