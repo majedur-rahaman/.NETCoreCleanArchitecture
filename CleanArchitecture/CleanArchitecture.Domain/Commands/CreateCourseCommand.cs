58a516c6c78e229e15cc03b837a128ed60b524f0
DESKTOP-GFB4VK6\ues <majed.cseru@gmail.com>
2025-03-10 21:57:05 -0400

    Commands, CommandHandlers, Updated Course Repository, Cleaned up IoC Container

---
+++ CleanArchitecture/CleanArchitecture.Domain/Commands/CreateCourseCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Commands
+{
+    public class CreateCourseCommand: CourseCommand
+    {
+        public CreateCourseCommand(string name, string description, string imageUrl)
+        { 
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
+    }
+}
