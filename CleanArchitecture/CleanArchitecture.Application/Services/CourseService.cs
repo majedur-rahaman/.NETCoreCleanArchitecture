d133a2505ba1a3ec63defc98b2e9beba245539b8
DESKTOP-GFB4VK6\ues <majed.cseru@gmail.com>
2025-03-11 00:03:16 -0400

    Course service Create functionality, updated CourseViewModel

--- CleanArchitecture/CleanArchitecture.Application/Services/CourseService.cs
+++ CleanArchitecture/CleanArchitecture.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Commands;
+using CleanArchitecture.Domain.Core.Bus;
 using CleanArchitecture.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -9,11 +11,24 @@ namespace CleanArchitecture.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseReporsitory;
-        public CourseService(ICourseRepository courseRepository)
+        private readonly ICourseRepository _courseReporsitory;
+        private readonly IMediatorHandler _bus;
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseReporsitory = courseRepository;
+            _bus = bus;
         }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                    courseViewModel.Name,
+                    courseViewModel.Description,
+                    courseViewModel.ImageUrl
+                );
+            _bus.SendCommand(createCourseCommand);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel()
