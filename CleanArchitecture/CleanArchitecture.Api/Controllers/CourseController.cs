b99e5f8652de312b22d0537ff9cfadaf4cb0c32e
DESKTOP-GFB4VK6\ues <majed.cseru@gmail.com>
2025-03-11 00:37:25 -0400

    Add Api Project and renamed controller

---
+++ CleanArchitecture/CleanArchitecture.Api/Controllers/CourseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+    }
+}
